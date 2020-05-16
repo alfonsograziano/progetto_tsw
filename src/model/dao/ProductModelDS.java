393fa1678285a96bef707a83b769bfd13f079783
lost99 <ivancadamo@gmail.com>
2020-05-16 14:38:40 +0200

    category fatta bene

--- src/model/dao/ProductModelDS.java
+++ src/model/dao/ProductModelDS.java
@@ -5,8 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.LinkedList;
+
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
