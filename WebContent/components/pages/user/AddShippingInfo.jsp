f67c8935ecfae81cec0884dcf35436ce49cf2746
99orazio <63792973+99orazio@users.noreply.github.com>
2020-05-20 15:05:29 +0200

    odio eclipse e tomcat

---
+++ WebContent/components/pages/user/AddShippingInfo.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<div>
+		<form action="AddShippingInfo" method="post">
+		<input type="hidden" name="action" value="insert"> 
+		
+		<label for="address">Address:</label><br> 
+		<input name="address" type="text" required placeholder="enter address"><br>
+		
+			<label for="state">State:</label><br> 
+		<input name="state" type="text" required placeholder="enter state"><br>
+		
+			<label for="zipCode">ZipCode:</label><br> 
+		<input name="zipCode" type="text" required placeholder="enter ZipCode"><br>
+		
+			<label for="city">City:</label><br> 
+		<input name="city" type="text" required placeholder="enter city"><br>
+		
+		<input type="submit" value="Add"><input type="reset" value="Reset">
+	</form>
+</div>
+</body>
+</html>
+
+
---
+++ src/control/shippingInfo/AddShippingInfo.java
@@ -0,0 +1,59 @@
+package control.shippingInfo;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.bean.ShippingInfo;
+import model.dao.ShippingInfoModelDS;
+import model.dao.ShippingModelDS;
+
+/**
+ * Servlet implementation class AddShippingInfo
+ */
+@WebServlet("ShippingInfo/AddShippingInfo")
+public class AddShippingInfo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddShippingInfo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String address = request.getParameter("address");
+		String state = request.getParameter("state");
+		String zip = request.getParameter("zipCode");
+		String city = request.getParameter("city");
+		
+		ShippingInfo x=new ShippingInfo();
+		x.setAddress(address);
+		x.setState(state);
+		x.setZipCode(zip);
+		x.setCity(city);
+		
+		ShippingInfoModelDS shippingInfoModel = new ShippingInfoModelDS();
+	
+			shippingInfoModel.add(x);
+	
+	}
+
+}
+
+
